c4430c013ad9b334f2b9b0b9117b7dc0c3bfb21b
Samantha González <samujm76@gmail.com>
2022-08-22 23:56:56 -0500

    Conversion de Objetos, creacion de otro hijo Escritor y una enumeracion

---
+++ src/domain/Gerente.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Gerente extends Empleado {
+
+    private String departamento; //No tiene subclases en este ejercicio, se pone como privado
+
+    public Gerente(String nombre, double sueldo, String departamento) {
+        super(nombre, sueldo);
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String obtenerDetalles(){
+        return super.obtenerDetalles() + ", Departamento: " + departamento;
+    }
+    
+    public String getDepartamento(){
+        return departamento = departamento;
+    }
+    
+    public void setDepartamento(String departamento){
+        this.departamento = departamento;
+    }
+}
